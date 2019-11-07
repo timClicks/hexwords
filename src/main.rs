867f7a08e42d9f6807b255090b604cdbf2e0b14d
Tim McNamara <tim.mcnamara@canonical.com>
2019-11-08 07:12:31 +1300

    Stream file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
+use std::io::{BufReader, BufRead};
 
 fn main() -> std::io::Result<()> {
     let f = File::open("/usr/share/dict/words")?;
-    let mut buf_reader = BufReader::new(f);
-    let mut words = String::new();
-    buf_reader.read_to_string(&mut words)?;
+    let reader = BufReader::new(f);
 
-    'lines: for line in words.lines() {
-        for byte in line.bytes() {
+    'lines: for line in reader.lines() {
+        let word = line.unwrap();
+        for byte in word.bytes() {
             match byte {
                 b'A' | b'B' | b'C' | b'D' | b'E' | b'F' |
                 b'a' | b'b' | b'c' | b'd' | b'e' | b'f' => continue,
@@ -17,8 +15,8 @@ fn main() -> std::io::Result<()> {
             }
         }
 
-        if line.len() > 2 {
-            println!("{}", line);
+        if word.len() > 2 {
+            println!("{}", word);
         }
     };
 
